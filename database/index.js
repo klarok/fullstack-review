3ddb3714db857a8f94d7ec5e39bd3ef6b3b1c126
klarok <karaylu@gmail.com>
2018-12-14 13:52:20 -0800

    Server, database running, can communicate
    	- Can insert JSON data into db by sending POST request to server

--- database/index.js
+++ database/index.js
@@ -1,16 +1,68 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+let db = mongoose.connection;
+db.once('open', () => {
+	console.log('Mongoose ready to go');
+});
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  name: String,
+  full_name: String,
+  owner: {
+		login: String,
+		id: Number,
+		avatar_url: String,
+		html_url: String,
+		repos_url: String
+	},
+  html_url: String,
+  description: String,
+  created_at: Date,
+  updated_at: Date,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+// let repo1 = new Repo ({
+// 	id: 1,
+// 	name: 'Fake Repo',
+// 	full_name: 'So Fake',
+// 	owner: {
+// 		login: 'faker',
+// 		id: 18,
+// 	},
+// 	description: 'Doesn\'t actually exist.'
+// });
+
+// repo1.save((err, repo1) => {
+// 	if (err) throw err;
+// 	console.log(repo1);
+// });
+
+// Repo.find((err, repos) => {
+// 	if (err) throw err;
+// 	console.log(repos);
+// });
+
+let save = (repoData) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  repoData.forEach(data => {
+  	let repo = new Repo(data);
+  	console.log('here\'s data', typeof data);
+  	console.log('here\'s repo', typeof repo);
+  	repo.save(err => {
+  		if (err) {
+  			console.log('Error saving repo to database', err);
+  		}
+  	});
+  });
+  //What about insertMany
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
+let {save} = require('../database');
 
+app.use(bodyParser());
+// app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
@@ -8,6 +12,8 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  save(req.body);
+  res.send('got a post request');
 });
 
 app.get('/repos', function (req, res) {
